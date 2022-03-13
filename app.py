881b0bfef6d2aa59183062e71361997452998a9d
LeanderCCC <slotosch.leander@outlook.de>
2022-03-13 14:01:38 +0100

    first

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, redirect, request, session
 from flask_session import Session
  
 app = Flask(__name__)
-app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+app.secret_key = "b_5#y2L'F4Q8z\n\xec]/"
 
 @app.route("/", methods=["GET", "POST"])
 def hello_world():
